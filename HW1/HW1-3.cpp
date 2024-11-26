264e808fa307b13fb64774d056557429856b79ae
Achitphon09 <sachittapol@gmail.com>
2024-11-26 12:10:39 +0700

    แก้ไขเงื่อนไข

--- HW1/HW1-3.cpp
+++ HW1/HW1-3.cpp
@@ -1,16 +1,24 @@
 #include <stdio.h>
+
 int main() {
     char frist_name[50] = "" ;
     char last_name[50] = "" ;
     
-    printf( "First Name: " ) ;
+    printf( "First Name:" ) ;
     scanf( "%s",&frist_name ) ;
 
     printf( "Last Name: " ) ;
     scanf( "%s",&last_name ) ;
 
-
-    printf( "%s %s TC, RMUTL, Chiang Mai, Thailand",frist_name,last_name ) ;
+    for(int i=0;i<3;i++) {
+        
+        if( i==2 ) {
+            if(frist_name[i] == 'C') {
+                frist_name[i] = 'C' + 1;
+            }
+        }
+    }
+    printf( "%s %ss TC, RMUTL, Chiang Mai, ThAailand",frist_name,last_name ) ;
     return 0 ;
 
 } 
