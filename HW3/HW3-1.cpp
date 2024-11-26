3d1fd2fed623714d72fb536d3835131554548af8
Achitphon09 <sachittapol@gmail.com>
2024-11-26 19:25:36 +0700

    แก้ไข codex

---
+++ HW3/HW3-1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+int main() {
+    int a = 10 ;
+    int b = 20 , c = 30, d= 40 ; 
+    
+    printf( "This is %d + %d = %d ", a , b , a + b ) ;
+    for( int i = 0 ; i < d - c ; i++ ) {
+        if( i < c ) {
+            printf( " Here " ) ;
+            if( i == 8 ) { 
+                printf( " This is 8. " ) ; 
+            }//end if
+        } //end if
+        printf( "ok" ) ;
+    } //end for
+    return 0 ; 
+}//end function
