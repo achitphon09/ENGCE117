e5dad3427fa960948b02731e6001843b70af8dfb
Achitphon09 <sachittapol@gmail.com>
2024-11-27 14:41:12 +0700

    create

--- HW1/HW1-1.cpp
+++ HW1/HW1-1.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h> 
-int main() {
-    printf( "Hello World" ) ; //แสดงผลข้อความผ่านทางจอภาพห
+
+int main() { //ฟังชั่นหลัก
+
+    printf( "Hello World" ) ; //แสดงผลข้อความผ่านทางจอภาพ
+
     return 0 ;
-}
+} //end function
