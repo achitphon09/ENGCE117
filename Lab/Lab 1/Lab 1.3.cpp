329b7fddf79c46798475d9646d443a43ce1258b4
Achitphon09 <sachittapol@gmail.com>
2024-11-29 23:24:17 +0700

    update

--- Lab/Lab 1/Lab 1.3.cpp
+++ Lab/Lab 1/Lab 1.3.cpp
@@ -1,10 +1,45 @@
 #include <stdio.h>
-
-int *GetSet( int * ) ;
+#include <stdlib.h> 
+/*int *GetSet( int * ) ;
 
 int main() {
     int *data, num ;
     data = GetSet( &num ) ;
     return 0 ;
 }//end function
+*/
+int *GetSet(int *num);
+
+int main() {
+    int *data, num ;
+    data = GetSet( &num ) ;
+
+    printf( "Number of elements : %d\n", num ) ;  // แสดงจำนวนสมาชิก
+    for ( int i = 0 ; i < num ; i++ ) {
+        printf( "%d ", data [ i ] ) ;  // แสดงสมาชิกในบรรทัดเดียว
+    }
+    printf( "\n" ) ;
+
+    free( data ) ; //คืนค่าหน่วยความจำที่จองไว้
+
+    return 0 ;
+}
+
+int *GetSet( int *num ) {
+    printf( "Enter the number of elements: " ) ;
+    scanf( "%d", num ) ;
+
+    int *data = (int *)malloc((*num) * sizeof(int));
+    if (data == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);  // ออกจากโปรแกรมหากจองหน่วยความจำไม่สำเร็จ
+    }
+
+    printf( "Enter the elements: " ) ;
+    for (int i = 0 ; i < *num ; i++ ) {
+        scanf( "%d", &data[ i ] ) ;
+    }
+
+    return data ;
+}
 
