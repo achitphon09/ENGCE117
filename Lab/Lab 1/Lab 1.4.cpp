ec80ec5e5de564f1a71263ab85b0b6993626075f
Achitphon09 <sachittapol@gmail.com>
2024-11-29 23:44:17 +0700

    update

---
+++ Lab/Lab 1/Lab 1.4.cpp
@@ -0,0 +1,40 @@
+/*#include <stdio.h>
+
+int GetSet( int [] ) ;
+
+int main() {
+    int *data, num ;
+    num = GetSet( &data ) ;
+    return 0 ;
+}//end function*/
+
+#include <stdio.h>
+
+int GetSet(int data[]) {
+    int n, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &data[i]);
+    }
+
+    return n;
+}
+
+int main() {
+    int num;
+    int data[100];  // กำหนดอาร์เรย์ขนาดคงที่
+
+    num = GetSet(data);
+
+    printf("Number of elements: %d\n", num);
+    for (int i = 0; i < num; i++) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
