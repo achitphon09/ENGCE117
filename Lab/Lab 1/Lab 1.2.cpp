438dd416a91a2e6a1267ae195b8e2d17051fc302
Achitphon09 <sachittapol@gmail.com>
2024-11-29 23:00:33 +0700

    update

--- Lab/Lab 1/Lab 1.2.cpp
+++ Lab/Lab 1/Lab 1.2.cpp
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
-void GetSet( int[], int *num ) ;
+void GetSet( int data[], int *num ) ;
 
 int main() {
-    int *data, num ;
-    GetSet( &data, &num ) ;
-    return 0;
-}// end function
+    int data[ 100 ] ;
+    int num ;
+    GetSet( data, &num ) ;
+    printf( "%d\n", num ) ; //แสดงจำนวนสมาชิกในเซ็ต
+
+    for ( int i = 0 ; i < num ; i++ ) {
+        printf( "%d ", data[ i ] ) ;
+    }
+    printf( "\n" ) ;
+    
+    return 0 ;
+}
+
+void GetSet( int data[ ], int *num ) {
+    scanf( "%d", num ) ;
+    for (int i = 0 ; i < *num ; i++ ) {
+        scanf( "%d", &data[ i ] ) ;
+    } 
+}
 
 
 
