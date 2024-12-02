7fa4c106b3002c14f935c1f2f588e00db692b142
Achitphon09 <sachittapol@gmail.com>
2024-12-02 22:33:29 +0700

    update

--- Lab/Lab 1/Lab 1.5.cpp
+++ Lab/Lab 1/Lab 1.5.cpp
@@ -14,50 +14,41 @@ void GetMatrix( int value[], int *row, int *col ) ;
 int main() {
     int data[ 100 ], m, n ;
     GetMatrix( data, &m, &n ) ;
-    
-    /*if ( m == 0 && n == 0 ) {
-        printf( "Matrix: (empty)\n" ) ;
-        return 0 ;
-    }// end if*/
-
-    printf( "Matrix (%dx%d): \n", m, n ) ;
-    for( int i = 0 ; i < m ; i++ ) {
-        for( int j = 0 ; j < n ; j++ ) {
-            printf( "%d ", data[ i * n + j ] ) ;
-        }//end for
-    printf( "\n" ) ;
-    }//end for
     return 0 ;
-}//end function 
+}
 
-void GetMatrix( int value[], int *row, int *col ) {
+ void GetMatrix( int value[], int *row, int *col ) {
     printf( "Enter the number of rows: " ) ;
     scanf( "%d", row ) ;
     printf( "Enter the number of columns: " ) ;
     scanf( "%d", col ) ; 
 
-    if ( ( *row < 0 || *col < 0 ) || ( *row == 0 && *col > 0 ) || ( *row > 0 && *col == 0 ) ) {
-        printf( "Error: Invalid matrix dimentions.\n" ) ;
-        *row = *col = 0 ;
-        return  ;
-    }//end if
-
-    if ( *row == 0 && *col == 0 ) {
-        printf( "Matrix : \n(empty)" ) ; 
-        return ; //end if 
+    if( *row == 0 && *col == 0 ) {
+        printf( "Matrix: (empty)\n" ) ;
     }
+    else if( ( *row == 0 && *col > 0 ) || ( *row > 0 && *col == 0 ) ) {
+        printf( "Error: Invalid matrix dimensions.\n" ) ;
+    }else{
+        printf( "Enter the elements of matrix: " ) ;
+        for( int i = 0 ; i < *row ; i++ ) {
+            for( int j = 0 ; j < *col ; j++ ) {
+                scanf( "%d", &value[ i * ( *col ) + j ]  ) ; 
+                if( getchar() == '\n' &&  i == *row - 1 && j == *col - 2 ) {
+					printf( "Error: Invalid matrix input" ) ;
+					return ;
+				}
+            }
+        }
 
-    printf( "Enter the elements of the matrix: " ) ;
-    int total = *row * *col ;
-    for( int i = 0 ; i < total ; i++ ) {
-        if( scanf( "%d", &value[ i ] ) != 1 ) {
-            printf( "Error: Incorrect number of elements.\n" ) ;
-            *row = *col = 0 ;
-            return ;
-        }//end if
-    }//end for
-}//end function
-
+        printf( "Matrix (%dx%d): \n", *row, *col ) ;
+        for( int i = 0 ; i < *row ; i++ ) {
+            for( int j = 0 ; j < *col ; j++ ) {
+                printf( "%d ", value[ i * ( *col ) + j ] ) ;
+            }
+            printf( "\n" ) ;
+        }
+    }
+ }    
     
 
 
--- Lab/Lab 1/Lab 1.6.cpp
+++ Lab/Lab 1/Lab 1.6.cpp
@@ -4,9 +4,22 @@ int main() {
     int *data, m, n ;
     data = GetMatrix( &m, &n ) ;
     return 0 ;
-}//end function*/
+}//end function
 
 #include <stdio.h>
+
+int GetMatrix( int matrix[], int *row, int * col ) ;  
+int main () {
+    int *data, m, n ;
+    
+    data = GetMatrix( &m, &n ) ;
+    return 0 ;
+
+}//end funtion
+*/
+
+
+
 
 
 
