c4f18521eb4f777a2af3bc93c9bf3edbff5bc161
Achitphon09 <sachittapol@gmail.com>
2024-12-07 20:46:23 +0700

    complete HW 4

--- HomeWork/HW4/HW 4-6.cpp
+++ HomeWork/HW4/HW 4-6.cpp
@@ -1,22 +1,70 @@
-#include <stdio.h>
+/*#include <stdio.h>
+#include <string.h>
+
 int main() {
 
     char    id[ 10 ] ;
+    char    decimal[ 50 ] ;
     int     h ;
-    float   salary ;
-    float   total ;   
+    int     salary ;
+    //float   total ;   
 
     printf( "Input the Employee ID(Max. 10 chars) :\n" ) ;
     scanf( "%s", id ) ;
     printf( "Input the working hrs :\n" ) ;
     scanf( "%d", &h ) ;
     printf( "Salary amount/hr :\n" ) ;
-    scanf( "%f", &salary ) ;
+    scanf( "%d", &salary ) ;
 
-    total = h * salary ;
+    float total = h * salary ;
     printf( "Expected Output :\n" ) ;
     printf( "Employee ID = %s \n", id ) ;
-    printf( "Salary = U$ %.2f", total ) ;
+    printf( "Salary = U$ %.2f \n", total ) ;
+
+    int len = strlen( decimal ) ;
+    for( int i = 0 ; i < len - 3 ; i++ ) {
+        if( i != 0 && ( len - i ) % 3 == 0 ) {
+            printf( "," ) ;
+        }//end if
+        printf( "%c" , decimal[ i ] ) ; 
+    }//end for
+    printf( "%s\n", strchr( decimal, '.' ) ) ;
+
     
     return 0 ;
-}
+}//end function*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char    id[ 11 ] ;
+    char    decimal[ 20 ] ;
+    int     h ;
+    int     salary ;
+
+    printf( "Input the Employee ID(Max. 10 chars) : \n" ) ;
+    scanf( "%s", id ) ;
+    printf( "Input the working hrs : \n" ) ;
+    scanf( "%d", &h ) ;
+    printf( "Salary amount/hr: \n" ) ;
+    scanf( "%d", &salary ) ;
+
+    float total = h * salary ;
+    sprintf( decimal, "%.2f", total ) ; 
+    printf( "Expected Output :\n" ) ;
+    printf( "Employees ID = %s\n", id ) ;
+    printf( "Salary = U$ " ) ;
+
+    int len = strlen( decimal ) ;
+
+    for (int i = 0 ; i < len - 3 ; i++ ) { 
+        if (i != 0 && ( len - i ) % 3 == 0 ) { 
+            printf( "," ) ;
+        }//end if
+        printf( "%c" , decimal[ i ] ) ; 
+    }//end for
+    
+    printf( "%s\n",  strchr( decimal, '.' ) ) ;
+    return 0 ;
+}//end function
