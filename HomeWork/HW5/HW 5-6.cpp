ab3b13fbe1f0e81268b429b65e361d2aa9a0725c
Achitphon09 <sachittapol@gmail.com>
2024-12-08 00:51:42 +0700

    update HW 5-6

---
+++ HomeWork/HW5/HW 5-6.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+
+    int number ;
+
+    printf( "Input :\n" ) ;
+    scanf( "%d", &number ) ;
+
+    if( number > 0 ) {
+        printf( "Positive" ) ;
+    } else {
+        printf( "Negative" ) ;
+    }
+    return 0 ;
+}
