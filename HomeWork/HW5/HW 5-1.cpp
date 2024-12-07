404b2276d8b6bcc742366649a588ea2bcd74fe67
Achitphon09 <sachittapol@gmail.com>
2024-12-07 22:21:33 +0700

    update HW 5

---
+++ HomeWork/HW5/HW 5-1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    char a, b, c, d ;
+
+    printf( "Test Case :\n" ) ;
+        scanf( "%c %c %c %c" , &a, &b, &c, &d ) ;
+        printf( "Result : %c %c %c %c", d, c, b, a ) ;
+   
+    return 0 ;
+}
